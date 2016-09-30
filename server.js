a74c436b92284af3a74b55faf2db3a896fa0df89
BhuvanaAdhi <buvimeera@gmail.com>
2016-09-30 19:49:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles={
-    'article-one':{
+    article_one:{
     title: "Web Development",
     heading: "Web Development",
     paratitle: "Basics of Web Development - Sep 28,2016",
@@ -15,8 +15,10 @@ var articles={
                 Web development is a broad term for the work involved in developing a web site for the Internet (World Wide Web) or intranet (a private network). Web development can range from developing the simplest static single page of plain text to the most complex web-based internet applications, electronic businesses, and social network services. A more comprehensive list of tasks to which web development commonly refers, may include web engineering, web design, web content development, client liaison, client-side/server-side scripting, web server and network security configuration, and e-commerce development. Among web professionals, "web development" usually refers to the main non-design aspects of building web sites: writing markup and coding. Most recently Web development has come to mean the creation of content management systems or CMS. These CMS can be made from scratch, proprietary or open source. In broad terms the CMS acts as middleware between the database and the user through the browser. A principle benefit of a CMS is that it allows non-technical people to make changes to their web site without having technical knowledge.
             </p>
             
-    `},
-    'article-two':{
+    `
+        
+    },
+    article_two:{
          title: "Web Development",
         heading: "Web Development Industry",
         paratitle: "Web Development as a Industry - Sep 28,2016",
@@ -32,7 +34,7 @@ Web development has also impacted personal networking and marketing. Websites ar
         </p>
         `
     },
-    'article-three':{
+    article_three:{
          title: "Web Development",
         heading: "Security Feature",
         paratitle: "Security Considerations - Sep 28,2016",
